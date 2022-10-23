41702198c418a51bafc71500079588d9b9c49bb0
Pelayo Trives <pelayotrives@gmail.com>
2022-10-23 22:52:18 +0200

    Upgrades implemented related to MongoDB.

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,7 @@
 //? Command ---> npm i mongoose (To connect our app to MongoDB Atlas).
 //? Command ---> npm i dotenv (To make our private keys invisible).
 //? Command ---> npm i axios (To request information).
+//? Command ---> npm i cors (To enable CORS).
 //? Command ---> npm i morgan (To make HTTP requests).
 //? Command ---> npm i opener (To open localhost easily and test our content).
 //? Command ---> "scripts": { "start": "nodemon src/index.js" } (To enable npm run start).
@@ -12,36 +13,57 @@
 //? Command ---> npm run start (To execute our main file).
 
 //! -----> Requirements
-// First, we require express, mongoose, our dotenv file and destructure console.log().
-// We assign the variable "express" to "app" and the port to "port".
-// Last const will make our app to listen to a port on a hosting service or port 9000 on a local environment.
+// First, we require express, mongoose... and destructure console.log().
 const { log } = console;
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 require("dotenv").config();
 
+// We assign the variable "express" to "app". We require our app to use express.json and cors.
+// Last const will make our app to listen to a port on a hosting service or port 9000 on a local environment.
 const app = express();
+app.use(express.json);
+app.use(cors());
 const port = process.env.PORT || 9000;
 
 //! -----> MongoDB
-// MongoDB connection method.
-const mongoDBConnect = async () => {
+// MongoDB connection method to MongoDB Atlas.
+// const mongoAtlasConnect = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_ATLAS_URI);
+//     log("Connected to MongoDB successfully!");
+//   } catch (error) {
+//     log("Error connecting to MongoDB: ", error);
+//     throw error;
+//   }
+// };
+
+// MongoDB connection method to MongoDB Compass.
+const mongoCompassConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_COMPASS_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     log("Connected to MongoDB successfully!");
   } catch (error) {
     log("Error connecting to MongoDB: ", error);
     throw error;
   }
 };
+
 // Now we invoke the function to connect.
-mongoDBConnect();
+// mongoAtlasConnect();
+mongoCompassConnect();
 
 //! -----> Routes
 app.get("/", (req, res) => {
-    res.send("Test.");
-})
+  res.send("Test.");
+});
 
 //! -----> Port listening
 // We make app to listen to the port.
-app.listen(process.env.PORT, () => log("Server is listening on port", `${process.env.PORT}`));
+app.listen(process.env.PORT, () =>
+  log("Server is listening on port", `${process.env.PORT}`)
+);
