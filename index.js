8cbdbd865b1a35dc5f4f3bed37352313843fc6f3
Pelayo Trives <pelayotrives@gmail.com>
2023-11-06 00:49:19 +0100

    feat: localStorage function updated. addNote logic fixed. v1 finished.

--- index.js
+++ index.js
@@ -35,46 +35,65 @@ const showToast = (message, color) => {
  */
 const addNote = () => {
   if (inputField.value.trim().length === 0) {
-    //! If there's no string, an alert is sent.
-    showToast("Empty string! Add your note!", "FF9966");
+    showToast("Empty string! Add your note!", "#FF9966");
   } else {
-    //! DOM manipulation. <li> is created, classes are added, value is appended.
-    const note = document.createElement("li");
-    note.classList.add("unchecked", "list-element");
-    note.style.cursor = "pointer";
-    const noteContent = document.createTextNode(inputField.value);
-    note.appendChild(noteContent);
-    //! DOM manipulation. <span> is created, innerHTML is appended, id is added.
-    //! Then, the <span> is appended to the <li>.
-    //! Then, the <li> is appended to the <ul>
-    //! Then, the <input> is cleared.
-    const cross = document.createElement("span");
-    cross.innerHTML =
-      '<i class="fa-solid fa-x fa-sm" style="color: #46009d;"></i>';
-    cross.setAttribute("id", "delete_button");
-    note.appendChild(cross);
+    //! We create a new note using createNoteElement with the text and state Unchecked.
+    const note = createNoteElement(inputField.value, false);
+    //! We add the new note to the list of notes.
     notesList.appendChild(note);
+    //! We clean the input field, save the note, update its visibility and send an alert.
     inputField.value = "";
+    saveNotes();
     updateNotesVisibility();
-
-    //! If the string is created, an alert is sent.
     showToast("Note added!", "#008000");
-
-    cross.addEventListener("click", () => {
-      //! DOM manipulation. <li> is removed.
-      note.remove();
-      updateNotesVisibility();
-      //! If the string is deleted, an alert is sent.
-      showToast("Note deleted!", "#FF0000");
-    });
-
-    note.addEventListener("click", () => {
-      //! If <li> is clicked and it is unchecked, it turns into checked (and viceversa).
-      note.classList.toggle("checked");
-      note.classList.toggle("unchecked");
-      showToast("Note edited!", "#FF0000");
-    });
   }
+};
+
+/**
+ * The `createNoteElement` function creates a note element with specified content and checked status,
+ * and adds event listeners for deleting and toggling the note.
+ * @param noteContent - The content of the note that will be displayed in the li element.
+ * @param isChecked - The `isChecked` parameter is a boolean value that determines whether the note
+ * element should be initially checked or unchecked. If `isChecked` is `true`, the note element will
+ * have the class "checked" added to it, otherwise it will have the class "unchecked" added to it.
+ * @returns The function `createNoteElement` returns a newly created `li` element with the specified
+ * note content and checked status.
+ */
+const createNoteElement = (noteContent, isChecked) => {
+  //! An element <li> is created
+  const note = document.createElement("li");
+  //! We add the class list-element and depending its state, checked or unchecked
+  note.classList.add(isChecked ? "checked" : "unchecked", "list-element");
+  note.style.cursor = "pointer";
+  //! We create a text string from the specified input value.
+  note.appendChild(document.createTextNode(noteContent));
+
+  //! We create a <span> element and we insert an icon into it. Then we add a special id and we append it to each <li> element.
+  const cross = document.createElement("span");
+  cross.innerHTML =
+    '<i class="fa-solid fa-x fa-sm" style="color: #46009d;"></i>';
+  cross.setAttribute("id", "delete_button");
+  note.appendChild(cross);
+
+  /* Adds a click event listener to the `cross` element, which is a delete button for a note. When the delete button is clicked, the
+  callback function is executed. It updates the visibility of <ul>, saves how many <li> are in the <ul> and shows an alert. */
+  cross.addEventListener("click", () => {
+    note.remove();
+    updateNotesVisibility();
+    saveNotes();
+    showToast("Note deleted!", "#FF0000");
+  });
+
+  /* Adds a click event listener to each note element in the list. When a note is clicked, the
+  callback function is executed and saves if the <li> is checked or unchecked in localStorage. */
+  note.addEventListener("click", () => {
+    note.classList.toggle("checked");
+    note.classList.toggle("unchecked");
+    saveNotes();
+  });
+
+  //! After this process, we return the full note.
+  return note;
 };
 
 /**
@@ -85,11 +104,44 @@ const clearNote = () => {
   //! If there's no string to clear, an alert is sent.
   if (inputField.value.trim().length === 0) {
     showToast("Nothing to clear!", "#FF9966");
-    //! If there's a string, an alert is sent and <input> is cleared.
   } else {
+    //! If there's a string, an alert is sent and <input> is cleared.
     inputField.value = "";
     showToast("Content cleared!", "#008000");
   }
+};
+
+/**
+ * The function `saveNotes` saves the notes from a list into the browser's local storage.
+ */
+const saveNotes = () => {
+  //! We initialize the array as empty.
+  const notes = [];
+  //! This will iterate along the notes.
+  document.querySelectorAll("#notes_list li").forEach((note) => {
+    //! This will ignore the delete icon when saving
+    const text = note.textContent.replace(/×$/, "").trim();
+    //! This verifies if the note is checked when saved.
+    const isChecked = note.classList.contains("checked");
+    //! Then the 2 variables are pushed into the array.
+    notes.push({ text, isChecked });
+  });
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
+
+/**
+ * The `loadNotes` function retrieves saved notes from local storage and creates note elements for each
+ * saved note.
+ */
+const loadNotes = () => {
+  //! The array is pared to be later on iterated.
+  const savedNotes = JSON.parse(localStorage.getItem("notes")) || [];
+  //! Each note is appended into the noteList with its own text and checked state.
+  savedNotes.forEach((noteObj) => {
+    const note = createNoteElement(noteObj.text, noteObj.isChecked);
+    notesList.appendChild(note);
+  });
+  updateNotesVisibility();
 };
 
 /**
@@ -100,11 +152,12 @@ const updateNotesVisibility = () => {
   notesList.style.display = notesList.children.length === 0 ? "none" : "flex";
 };
 
-//! Event listener that makes the button add work.
-//! Event listener that updates the <ul> visibility when the users get into the page.
+//! Event listener that updates the <ul> visibility when the users get into the page and shows the notes saved in localStorage.
 document.addEventListener("DOMContentLoaded", function () {
+  loadNotes();
   updateNotesVisibility();
 });
+//! Event listener that makes the button add work.
 addButton.addEventListener("click", addNote);
 //! Event listener that listens for the "keyup" event, triggered when a key is released and adds a note if you hit enter.
 inputField.addEventListener("keyup", function (e) {
@@ -113,4 +166,3 @@ inputField.addEventListener("keyup", function (e) {
     addNote();
   }
 });
-
